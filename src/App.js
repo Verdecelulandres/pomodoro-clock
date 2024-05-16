6cfe677130b1a6c53f8d36f5279fc9c7d9c69e10
AndresLa <andres.laverde@protonmail.com>
2024-05-16 15:43:34 -0700

    countdown working for session

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faAngleUp, faPlay, faPause, faRepeat } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,14 +8,80 @@ function App() {
   const initialVals = [false, 25, 5];
   const[sessionlength, setSessionLength] = useState(initialVals[1]);
   const[breakLength, setBreakLength] = useState(initialVals[2]);
-  const[currentTime, setCurrentTime] = useState('25:00');
+  const[currentTime, setCurrentTime] = useState(initialVals[1]+':00');
   const[isOn, setIsOn] = useState(initialVals[0]);
+  const[isRunning, setIsRunning] = useState(initialVals[0]);
+  const[isBreak, setIsBreak] = useState(initialVals[0]);
+
 
  
   function resetVals() {
     setIsOn(initialVals[0]);
     setSessionLength(initialVals[1]);
+    setCurrentTime(initialVals[1] + ':00');
     setBreakLength(initialVals[2]);
+    setIsRunning(initialVals[0]);
+    setIsBreak(initialVals[0]);
+  }
+  useEffect(() => {
+    let timer;
+    if (isOn && isRunning) {
+      timer = setTimeout(() => countDown(currentTime), 1000);
+    }
+    return () => clearTimeout(timer);
+  }, [isOn, isRunning, currentTime]);
+
+  useEffect(() => {
+    if (!isOn && !isRunning) {
+      setCurrentTime(`${sessionlength}:00`);
+      //console.log('We are in use effect');
+    }
+  }, [sessionlength, isOn, isRunning]);
+
+  // Modify displayTime function
+  function displayTime() {
+    if (isOn || isRunning) {
+      //console.log('timer should be on');
+      return currentTime;
+    } else if(!isRunning){
+      //console.log('timer should be paused');
+      return sessionlength + ':00';
+    }
+  }
+  
+  function breakOrStudy() {
+    if(isBreak) {
+      return 'Break time!';
+    }
+    else {
+      return 'Study Session';
+    }
+  }
+
+  function countDown(time){
+    let minsAndSec = time.split(':');
+    let min = minsAndSec[0];
+    let sec = minsAndSec[1];
+    if(sec === '00'){
+      if(min === '00'){
+        //play alarm
+        //start break counter
+        setIsBreak(!isBreak); //toggles break or study
+        if(isBreak){  //If we reach the end and is a break we change to session
+          setCurrentTime(sessionlength + ':00');
+        } else {
+          setCurrentTime(breakLength + ':00');// If session ended we start break
+        }
+        return;
+      } else {
+        min = eval(min -1 ).toString();
+        sec = '59';
+      }
+    } else {
+      sec = eval(sec - 1).toString();
+    }
+    let newTime = min.padStart(2,'0') + ':' + sec.padStart(2,'0');
+    setCurrentTime(newTime);
   }
 
   return (
@@ -26,11 +92,11 @@ function App() {
         <div className='clock-container row'>  
           <div className='session-container col-8'>
             <div className='current-session'>
-              <div id='timer-label'>Current Session</div>
-              <div id='time-left'>{sessionlength + ':00'}</div>
+              <div id='timer-label'>{breakOrStudy()}</div>
+              <div id='time-left'>{displayTime()}</div>
             </div>
             <div className='session-controls'>
-              <button id='start_stop' onClick={()=>setIsOn(!isOn)}><FontAwesomeIcon icon={faPlay} /><FontAwesomeIcon icon={faPause} /></button>
+              <button id='start_stop' onClick={()=>{setIsOn(!isOn); setIsRunning(true)}}><FontAwesomeIcon icon={faPlay} /><FontAwesomeIcon icon={faPause} /></button>
               <button id='reset'onClick={resetVals}><FontAwesomeIcon icon={faRepeat} /></button>
             </div>
           </div>
@@ -40,16 +106,20 @@ function App() {
             
               <div id='session-label'>Session Length</div>
               <div className='length-container'>
-                <button id='session-decrement' onClick={() => setSessionLength(sessionlength-1)}><FontAwesomeIcon icon={faAngleDown}/></button>
+                <button id='session-decrement' onClick={() => {
+                  if(sessionlength > 1 && !isOn ){setSessionLength(sessionlength-1); setIsRunning(false)}}}><FontAwesomeIcon icon={faAngleDown}/></button>
                 <div id='session-length'>{sessionlength}</div>
-                <button id='session-increment'onClick={() => setSessionLength(sessionlength+1)}><FontAwesomeIcon icon={faAngleUp}/></button>
+                <button id='session-increment'onClick={() => {
+                  if(sessionlength < 60 && !isOn){setSessionLength(sessionlength+1); setIsRunning(false)}}}><FontAwesomeIcon icon={faAngleUp}/></button>
               </div>
             
               <div id='break-label'>Break Length</div>
               <div className='length-container'>
-                <button id='break-decrement' onClick={()=>setBreakLength(breakLength-1)}><FontAwesomeIcon icon={faAngleDown}/></button>
+                <button id='break-decrement' onClick={()=>{
+                  if(breakLength > 1 && !isOn) {setBreakLength(breakLength-1)}}}><FontAwesomeIcon icon={faAngleDown}/></button>
                 <div id='break-length'>{breakLength}</div>
-                <button id='break-increment' onClick={()=>setBreakLength(breakLength+1)}><FontAwesomeIcon icon={faAngleUp} /></button>
+                <button id='break-increment' onClick={()=>{
+                  if(breakLength < 60 && !isOn) {setBreakLength(breakLength+1)}}}><FontAwesomeIcon icon={faAngleUp} /></button>
               </div>
           </div>
         </div>
