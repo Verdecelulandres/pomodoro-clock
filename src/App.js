abddd37fdd0cc4decb34ae11b84670220946c423
AndresLa <andres.laverde@protonmail.com>
2024-05-16 11:31:27 -0700

    Add reset values functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import React from 'react';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faAngleUp, faPlay, faStop, faRepeat } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleUp, faPlay, faPause, faRepeat } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  const[sessionlength, setSessionLength] = useState(25);
-  const[breakLength, setBreakLength] = useState(5);
+  const initialVals = [false, 25, 5];
+  const[sessionlength, setSessionLength] = useState(initialVals[1]);
+  const[breakLength, setBreakLength] = useState(initialVals[2]);
+  const[currentTime, setCurrentTime] = useState('25:00');
+  const[isOn, setIsOn] = useState(initialVals[0]);
 
+ 
+  function resetVals() {
+    setIsOn(initialVals[0]);
+    setSessionLength(initialVals[1]);
+    setBreakLength(initialVals[2]);
+  }
 
   return (
     <div className="App">
@@ -18,11 +27,11 @@ function App() {
           <div className='session-container col-8'>
             <div className='current-session'>
               <div id='timer-label'>Current Session</div>
-              <div id='time-left'>25:00</div>
+              <div id='time-left'>{sessionlength + ':00'}</div>
             </div>
             <div className='session-controls'>
-              <button id='start_stop'><FontAwesomeIcon icon={faPlay} /><FontAwesomeIcon icon={faStop} /></button>
-              <button id='reset'><FontAwesomeIcon icon={faRepeat} /></button>
+              <button id='start_stop' onClick={()=>setIsOn(!isOn)}><FontAwesomeIcon icon={faPlay} /><FontAwesomeIcon icon={faPause} /></button>
+              <button id='reset'onClick={resetVals}><FontAwesomeIcon icon={faRepeat} /></button>
             </div>
           </div>
           
